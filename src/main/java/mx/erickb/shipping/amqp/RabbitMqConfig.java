73eed407e978093492ddcf5595db2c356eeee91e
Erick Blanco <erick.jassi.bs@gmail.com>
2020-08-22 16:28:07 -0600

    RabbitMq config variables moved into properties file

--- src/main/java/mx/erickb/shipping/amqp/RabbitMqConfig.java
+++ src/main/java/mx/erickb/shipping/amqp/RabbitMqConfig.java
@@ -7,36 +7,45 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:amqp.properties")
 public class RabbitMqConfig {
 
-    public static final String EXCHANGE_NAME = "db-exchange";
-    public static final String QUEUE_NAME = "db-queue";
-    public static final String ROUTING_KEY = "#";
+    @Value("${exchange-name}")
+    private String exchangeName;
+
+    @Value("${queue-name}")
+    private String queueName;
+
+    @Value("${routing-key}")
+    private String routingKey;
 
     @Bean
     Queue queue() {
-        return new Queue(QUEUE_NAME, false);
+        return new Queue(queueName, false);
     }
 
     @Bean
     TopicExchange exchange() {
-        return new TopicExchange(EXCHANGE_NAME);
+        return new TopicExchange(exchangeName);
     }
 
     @Bean
     Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
     }
 
     @Bean
     RabbitTemplate template(ConnectionFactory factory) {
         RabbitTemplate template = new RabbitTemplate(factory);
-        template.setRoutingKey(ROUTING_KEY);
-        template.setExchange(EXCHANGE_NAME);
+        template.setRoutingKey(routingKey);
+        template.setExchange(exchangeName);
         template.setReplyTimeout(10000);
 
         return template;
@@ -46,7 +55,7 @@ public class RabbitMqConfig {
     SimpleMessageListenerContainer container(ConnectionFactory connectionFactory) {
         final SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(QUEUE_NAME);
+        container.setQueueNames(queueName);
         return container;
     }
 }
